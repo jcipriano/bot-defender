89ea81faa6d848f621e0f65c5e468904bd315860
jcipriano <>
2024-03-20 16:12:40 -0700

    code foramtting

--- src/edge-functions/hello-world.ts
+++ src/edge-functions/hello-world.ts
@@ -3,7 +3,7 @@
 const pxParams = {
   px_app_id: Netlify.env.get("PX_APP_ID"),
   px_auth_token: Netlify.env.get("PX_AUTH_TOKEN"),
-  px_cookie_secret: Netlify.env.get("PX_COOKIE_SECRET"),
+  px_cookie_secret: Netlify.env.get("PX_COOKIE_SECRET")
 };
 
 export default () => new Response('Hello, world! px_app_id:' + pxParams.px_app_id + ', px_auth_token:' + pxParams.px_auth_token + ', px_cookie_secret:' + pxParams.px_cookie_secret);
